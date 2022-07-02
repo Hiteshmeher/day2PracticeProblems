c580d90a5b352390ae3b2bdd14911e5968fff767
Hitesh Kumar Meher <107098715+Hiteshmeher@users.noreply.github.com>
2022-07-02 20:49:32 +0530

    day2 practice problems

--- src/day2PracticeProblems/Palindrome_Number.java
+++ src/day2PracticeProblems/Palindrome_Number.java
@@ -12,14 +12,17 @@ public class Palindrome_Number {
 		Scanner sc = new Scanner(System.in);
 		int inputNumber = sc.nextInt();
 		int a = inputNumber;
+		int temp = a;
 		while (a > 0) {
 			lastDigit = a % 10;
-			a = a / 10;
 			reverseNum = reverseNum * 10 + lastDigit;
+			a = a / 10;
 		}
+		System.out.println("The input number is " + temp);
+
 		System.out.println("The reverse number is " + reverseNum);
 
-		if (a == reverseNum) {
+		if (temp == reverseNum) {
 			System.out.println("Number is palindrome ");
 		} else {
 			System.out.println("Number is not palindrome");
